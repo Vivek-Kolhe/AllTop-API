8b2f143e7a76ace46f13b49a418d2d6b347f5b86
Vivek-Kolhe <61515675+Vivek-Kolhe@users.noreply.github.com>
2020-10-16 15:17:04 +0530

    fetches viral articles from alltop.com

--- alltop.py
+++ alltop.py
@@ -9,7 +9,7 @@ def get_data(category):
     response = requests.get(url)
     soup = BeautifulSoup(response.text, "html.parser")
     news_containers = soup.findAll(class_ = "col-xs-12 col-md-4")
-    jsonData = {"category" : category, "data" : []}
+    newsDictionary = {"category" : category, "data" : []}
 
     for i in range(len(news_containers)):
         news_site_tag = news_containers[i].find("a", style = "color: #ea2f10; font-size: 17px;")        # for news source sites
@@ -28,5 +28,26 @@ def get_data(category):
                 content = None
             temp_data.append({"content" : content, "title" : news_title, "url" : url})
         
-        jsonData["data"].append({"newsFrom" : news_site, "newsUrl" : news_url, "newsData" : temp_data})
-    return jsonData
+        newsDictionary["data"].append({"newsFrom" : news_site, "newsUrl" : news_url, "newsData" : temp_data})
+    return newsDictionary
+
+def viral():
+	data = []
+	for page in range(1, 4):                                                                            # for fetching articles from only 3 pages
+		url = f"https://alltop.com/viral/page/{page}"
+		response = requests.get(url)
+		soup = BeautifulSoup(response.text, "html.parser")
+		articles = soup.findAll(class_ = "row-fluid row-featured")
+		for i in range(len(articles)):
+			title = articles[i].find(class_ = "post-title").text
+			posted = articles[i].find(class_ = "posted-by").text
+			posted = posted.split(" / ")
+			posted_by, posted_on = posted[0][10:], posted[-1]
+			thumbnail = articles[i].find("img")["src"]
+			content = articles[i].find("p").text
+			readmore = articles[i].find(class_ = "read-more")["href"]
+
+			data.append({"content" : content, "title" : title, "postedBy" : posted_by, "postedOn" : posted_on, "thumbnail" : thumbnail, "readMoreUrl" : readmore})
+
+	articleDictionary = {"category" : "viral", "articleCount" : len(data), "data" : data}
+	return articleDictionary
